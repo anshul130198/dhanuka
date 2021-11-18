fad59465b97e4e4525e2d12cfae6f6752bc7cd76
Anshul Goyal <goyal.anshul@tftus.com>
2021-11-18 14:05:33 +0530

    minor changes

--- src/app/components/dialog/location/location.component.ts
+++ src/app/components/dialog/location/location.component.ts
@@ -1,3 +1,4 @@
+import { MainService } from './../../../services/main.service';
 import { Component, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
@@ -12,7 +13,10 @@ export class LocationDialogeComponent {
   form: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
-    public dialogRef: MatDialogRef<LocationDialogeComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+    public dialogRef: MatDialogRef<LocationDialogeComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private mainService: MainService) {
+    this.askForLocation();
 
     this.form = this.formBuilder.group({
       name: ['', [Validators.required, Validators.maxLength(100)]],
@@ -21,9 +25,18 @@ export class LocationDialogeComponent {
     })
   }
 
-  get f(){  
-    return this.form.controls;  
-  }  
+  get f() {
+    return this.form.controls;
+  }
+
+  askForLocation() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      alert('Location accessed');
+      console.log(position.coords);
+    }, function () {
+      alert('User not allowed')
+    }, { timeout: 10000 })
+  }
 
   onClose() {
     this.dialogRef.close();
@@ -31,6 +44,15 @@ export class LocationDialogeComponent {
 
   submit() {
     console.log(this.form.value);
+    let obj = {
+      name: this.form.get('name').value,
+      mobile: this.form.get('mobile').value,
+      location: this.form.get('location').value,
+      userLocation: ''
+    }
+    this.mainService.sendLocationDetails(obj).subscribe(res=>{
+
+    })
     this.dialogRef.close(this.form.value);
   }
 
--- src/app/components/valid/valid.component.ts
+++ src/app/components/valid/valid.component.ts
@@ -1,8 +1,9 @@
 import { MainService } from './../../services/main.service';
 import { LocationDialogeComponent } from './../dialog/location/location.component';
-import { Component,  OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { KeyValue } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-valid',
@@ -12,34 +13,32 @@ import { KeyValue } from '@angular/common';
 export class ValidComponent {
 
   locationDialogRef: MatDialogRef<LocationDialogeComponent>;
+  showDataTable: boolean = false;
 
   constructor(private dialog: MatDialog,
-    private mainService: MainService) {
+    private mainService: MainService,
+    private router: Router) {
     this.userStartPopUp();
-
-
+    console.log(this.router.url)
+    this.getProductData(this.router.url);
   }
-
 
   data = {
     'Mfg.by': 'Dhanuka Agritech Ltd',
     'Registration No.': 'test',
     'Batch No.': 'jvhbn',
     'Exp Date': '8564789',
-    'Cautionary Symbol': 
-      {
-        name:'Poison',
-        imgurl:'../../../assets/icons/Poison.svg'
-      },
-      'Antidote Statement':'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
-      'Customer Care No.': '1800-102-1022',
-      'Label information':'#1',
-      'Leaflet information':'#2',
-      'Product video':'#3',
+    'Cautionary Symbol':
+    {
+      name: 'Poison',
+      imgurl: '../../../assets/icons/Poison.svg'
+    },
+    'Antidote Statement': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+    'Customer Care No.': '1800-102-1022',
+    'Label information': '#1',
+    'Leaflet information': '#2',
+    'Product video': '#3',
   }
- 
-  message = 'Thank you for scanning Geniune Dhanuka Products Or QR Details';
-
 
   userStartPopUp() {
     this.locationDialogRef = this.dialog.open(LocationDialogeComponent, {
@@ -51,21 +50,20 @@ export class ValidComponent {
     });
 
     this.locationDialogRef.afterClosed().subscribe(result => {
-      console.log(result)
-      navigator.geolocation.getCurrentPosition(function (position) {
-        alert('Location accessed');
-        console.log(position);
-      }, function () {
-        alert('User not allowed')
-      }, { timeout: 10000 })
-      //this.claimPointsPopUp();
-      // let check = localStorage.getItem('newUser')
-      // if (check && check != 'false') {
+      console.log(result);
+      this.showDataTable = true;
     })
   }
 
-  originalOrder = (a: KeyValue<number,string>, b: KeyValue<number,string>): number => {
+  originalOrder = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
     return 0;
+  }
+
+  getProductData(url) {
+    this.mainService.getProductData(url).subscribe(res => {
+      console.log(res);
+    })
+
   }
 
 
