362bec3d68bbc1a4ac0f1455f3651b2aa6a047f5
Anush-TFT <sharma.anush@thinkfuture.us>
2021-11-18 11:38:52 +0530

    added mobile view

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,33 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
-  styleUrls: ['header.component.scss']
+  styleUrls: ['header.component.scss'],
+  animations: [
+		trigger('onOff', [
+			transition('void => *', [style({
+				opacity: 0,
+				transform: 'translateX(-10%)'
+			}),
+			animate(400)
+			]),
+      transition(':leave', [style({
+				opacity: 0,
+				transform: 'translateX(10%)'
+			}),
+			animate(400)
+			])
+		])
+	]
 })
-export class HeaderComponent {
 
+
+export class HeaderComponent {
+  showNav=false;
+
+  toggleNav(){
+    this.showNav=!this.showNav;
+  }
 }
