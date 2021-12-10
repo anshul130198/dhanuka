f1350e6892fe10b73c0608e00521e4f19749b940
Anush-TFT <sharma.anush@thinkfuture.us>
2021-12-10 11:51:48 +0530

    bug fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // baseUrl : 'https://192.168.23.44:81/',
-  baseUrl : 'https://203.110.83.67:81/'
-  // baseUrl : 'https://10.240.251.18:81/'
+  // baseUrl : 'https://203.110.83.67:81/'
+  baseUrl : 'https://10.240.251.18:81/'
 };
